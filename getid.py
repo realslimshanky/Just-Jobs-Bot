c6e834b688c2f70dbf16ccfadd37d889b9d733ea
Shashank Kumar <abc@shanky.xyz>
2017-08-24 22:57:41 +0530

    Version 0.2

---
+++ getid.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction
+import logging
+import re
+import configparser
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+updater=Updater(token=config['BOT']['TOKEN'])
+dispatcher=updater.dispatcher
+
+def getID(bot, update):
+    print("Channel ID = " + str(update.channel_post.chat.id))
+
+dispatcher.add_handler(MessageHandler(Filters.text, getID))
+
+updater.start_polling()
--- justjobs.py
+++ justjobs.py
@@ -1,13 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ChatAction
-import logging,requests
+import logging
 import re
 import configparser
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('../config.ini')
 
 updater=Updater(token=config['BOT']['TOKEN'])
 dispatcher=updater.dispatcher
